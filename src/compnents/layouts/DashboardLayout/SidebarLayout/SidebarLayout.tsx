0ded0fa57d1dd402e9262d8c6fde5916b1c8b395
frhn-06 <farhanmunif233@gmail.com>
2026-07-30 12:24:07 +0700

    fix: refactor sidebar layout

--- src/compnents/layouts/DashboardLayout/SidebarLayout/SidebarLayout.tsx
+++ src/compnents/layouts/DashboardLayout/SidebarLayout/SidebarLayout.tsx
@@ -1,6 +1,6 @@
 import cn from "@/utils/cn";
 import listsidebar from "./listSidebar"
-import { SetStateAction } from "react";
+import { SetStateAction, useEffect, useRef } from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { signOut } from "next-auth/react";
@@ -24,8 +24,24 @@ const SidebarLayout = (props: TypeProps) => {
 
     const router = useRouter();
 
+    const s = useRef<boolean>(true)
+    useEffect(() => {
+      if(s.current) {
+        console.log(router.pathname)
+      }
+      s.current = false
+    })
+
     return (
         <div className="relative">
+
+      
+      
+{/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+          
+
+          {/* sidebar dekstop */}
+
           <div className={cn("bg-red-800 h-full overflow-hidden transition-[width] duration-300 hidden lg:block", sidebarAktif ? "w-64" : "w-14")}>
 
             <div className="h-20 p-1">
@@ -33,42 +49,64 @@ const SidebarLayout = (props: TypeProps) => {
 
               </div>
             </div>
-      
+
             <div className="flex flex-col">
               {type === "admin" ? 
-                listsidebar.admin.map((sidbar) => (
-                  <Link key={sidbar.id} href={sidbar.href} className="relative p-2 flex items-center hover:bg-red-500 active:bg-red-400 transition">
-                    <div className={cn("w-1 bg-white h-full absolute left-0 rounded-r-xl", router.pathname === sidbar.href ? "block" : "hidden")} />
-
-                    <div className="w-8 h-8 text-white">
-                      {sidbar.icon}
+                listsidebar.admin.map((sidebar) => (
+                  <div key={sidebar.id}>
+                    <div className="px-3 py-2">
+                      <span className={cn("font-semibold text-white whitespace-nowrap transition-all duration-300", sidebarAktif? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10")}>
+                        {sidebar.label}
+                      </span>
                     </div>
-              
-                    <span className={cn("text-white ml-2 whitespace-nowrap transition-all duration-300", sidebarAktif? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10")}>
-                      {sidbar.label}
-                    </span>
-                  </Link>
+                    <div className="flex flex-col">
+                      {sidebar.items.map((item) => (
+                        <Link key={item.id} href={`${item.href}`} className={cn("text-gray-200 relative px-3 py-2 flex items-center hover:bg-red-500 active:bg-red-400 transition", router.pathname.startsWith(item.href) && "bg-red-900")}>
+                          <div className={cn("absolute w-1 rounded-r-xl bg-gray-200 top-0 left-0 h-full", router.pathname.startsWith(item.href) ? "block" : "hidden")} />
+
+                          <div className="w-6 h-6">
+                            {item.icon}
+                          </div>
+                      
+                          <span className={cn("text-sm ml-2 whitespace-nowrap transition-all duration-300", sidebarAktif? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10")}>
+                            {item.label}
+                          </span>
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
                 )) : 
-                listsidebar.petugas.map((sidbar) => (
-                  <Link key={sidbar.id} href={sidbar.href} className="relative p-2 flex items-center hover:bg-red-500 active:bg-red-400 transition">
-                    <div className={cn("w-1 bg-white h-full absolute left-0 rounded-r-xl", router.pathname === sidbar.href ? "block" : "hidden")} />
-
-                    <div className="w-8 h-8 text-white">
-                      {sidbar.icon}
+                listsidebar.petugas.map((sidebar) => (
+                  <div key={sidebar.id}>
+                    <div className="px-3 py-2">
+                      <span className={cn("font-semibold text-white whitespace-nowrap transition-all duration-300", sidebarAktif? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10")}>
+                        {sidebar.label}
+                      </span>
                     </div>
-              
-                    <span className={cn("text-white ml-2 whitespace-nowrap transition-all duration-300", sidebarAktif? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10")}>
-                      {sidbar.label}
-                    </span>
-                  </Link>
+                    <div className="flex flex-col">
+                      {sidebar.items.map((item) => (
+                        <Link key={item.id} href={`${item.href}`} className={cn("text-gray-200 relative px-3 py-2 flex items-center hover:bg-red-500 active:bg-red-400 transition", router.pathname.startsWith(item.href) && "bg-red-900")}>
+                          <div className={cn("absolute w-1 rounded-r-xl bg-gray-200 top-0 left-0 h-full", router.pathname.startsWith(item.href) ? "block" : "hidden")} />
+
+                          <div className="w-6 h-6">
+                            {item.icon}
+                          </div>
+                      
+                          <span className={cn("text-sm ml-2 whitespace-nowrap transition-all duration-300", sidebarAktif? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10")}>
+                            {item.label}
+                          </span>
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
                 ))
               } 
               <Link href="/logout" onClick={(e) => {
                 e.preventDefault();
                 signOut();
-              }} className="relative p-2 flex items-center hover:bg-red-500 active:bg-red-400 transition">
-                <div className="w-8 h-8 text-white">
-                  <MdLogout  className="w-8 h-8" />
+              }} className="font-semibold relative px-3 py-2 flex items-center hover:bg-red-500 active:bg-red-400 transition">
+                <div className="w-6 h-6 text-white">
+                  <MdLogout className="w-6 h-6" />
                 </div>
             
                 <span className={cn("text-white ml-2 whitespace-nowrap transition-all duration-300", sidebarAktif? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10")}>
@@ -79,7 +117,15 @@ const SidebarLayout = (props: TypeProps) => {
           </div>
             
 
+{/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
 
+
+
+
+{/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+          
+
+          {/* sidebar mobile */}
       
           <div className={cn("z-10 fixed top-0 bottom-0 left-0 w-64 bg-red-800 transition-transform duration-300 lg:hidden", sidebarAktif ? "-translate-x-full" : "-translate-x-0")}>
             <div className="h-24 flex items-center px-4">
@@ -95,31 +141,53 @@ const SidebarLayout = (props: TypeProps) => {
             
             <div className="flex flex-col">
               {type === "admin" ? 
-                listsidebar.admin.map((sidbar) => (
-                  <Link key={sidbar.id} href={sidbar.href} className="relative p-2 flex items-center hover:bg-red-500 active:bg-red-400 transition">
-                    <div className={cn("w-1 bg-white h-full absolute left-0 rounded-r-xl", router.pathname === sidbar.href ? "block" : "hidden")} />
-
-                    <div className="w-8 h-8 text-white">
-                      {sidbar.icon}
+                listsidebar.admin.map((sidebar) => (
+                  <div key={sidebar.id}>
+                    <div className="px-3 py-2">
+                      <span className={cn("font-semibold text-white whitespace-nowrap transition-all duration-300")}>
+                        {sidebar.label}
+                      </span>
                     </div>
-              
-                    <span className="text-white ml-2 whitespace-nowrap transition-all duration-300">
-                      {sidbar.label}
-                    </span>
-                  </Link>
+                    <div className="flex flex-col">
+                      {sidebar.items.map((item) => (
+                        <Link key={item.id} href={`${item.href}`} className={cn("text-gray-200 relative px-3 py-2 flex items-center hover:bg-red-500 active:bg-red-400 transition", router.pathname.startsWith(item.href) && "bg-red-900")}>
+                          <div className={cn("absolute w-1 rounded-r-xl bg-gray-200 top-0 left-0 h-full", router.pathname.startsWith(item.href) ? "block" : "hidden")} />
+
+                          <div className="w-6 h-6">
+                            {item.icon}
+                          </div>
+                      
+                          <span className={cn("text-sm ml-2 whitespace-nowrap transition-all duration-300")}>
+                            {item.label}
+                          </span>
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
                 )) :
-                listsidebar.petugas.map((sidbar) => (
-                  <Link key={sidbar.id} href={sidbar.href} className="relative p-2 flex items-center hover:bg-red-500 active:bg-red-400 transition">
-                    <div className={cn("w-1 bg-white h-full absolute left-0 rounded-r-xl", router.pathname === sidbar.href ? "block" : "hidden")} />
-
-                    <div className="w-8 h-8 text-white">
-                      {sidbar.icon}
+                listsidebar.petugas.map((sidebar) => (
+                  <div key={sidebar.id}>
+                    <div className="px-3 py-2">
+                      <span className={cn("font-semibold text-white whitespace-nowrap transition-all duration-300")}>
+                        {sidebar.label}
+                      </span>
                     </div>
-              
-                    <span className="text-white ml-2 whitespace-nowrap transition-all duration-300">
-                      {sidbar.label}
-                    </span>
-                  </Link>
+                    <div className="flex flex-col">
+                      {sidebar.items.map((item) => (
+                        <Link key={item.id} href={`${item.href}`} className={cn("text-gray-200 relative px-3 py-2 flex items-center hover:bg-red-500 active:bg-red-400 transition", router.pathname.startsWith(item.href) && "bg-red-900")}>
+                          <div className={cn("absolute w-1 rounded-r-xl bg-gray-200 top-0 left-0 h-full", router.pathname.startsWith(item.href) ? "block" : "hidden")} />
+
+                          <div className="w-6 h-6">
+                            {item.icon}
+                          </div>
+                      
+                          <span className={cn("text-sm ml-2 whitespace-nowrap transition-all duration-300")}>
+                            {item.label}
+                          </span>
+                        </Link>
+                      ))}
+                    </div>
+                  </div>
                 ))
               }
               <Link href="/logout" onClick={(e) => {
@@ -130,13 +198,16 @@ const SidebarLayout = (props: TypeProps) => {
                   <MdLogout  className="w-8 h-8" />
                 </div>
             
-                <span className={cn("text-white ml-2 whitespace-nowrap transition-all duration-300", sidebarAktif? "opacity-100 translate-x-0" : "opacity-0 -translate-x-10")}>
+                <span className={cn("text-white ml-2 whitespace-nowrap transition-all duration-300")}>
                   Logout
                 </span>
               </Link>
               
             </div>
           </div>
+
+{/* //////////////////////////////////////////////////////////////////////////////////////////////////////////////// */}
+          
         </div>
     )
 }
--- src/compnents/layouts/DashboardLayout/SidebarLayout/listSidebar.tsx
+++ src/compnents/layouts/DashboardLayout/SidebarLayout/listSidebar.tsx
@@ -1,71 +1,179 @@
 import { FaUsers, FaUserTie } from "react-icons/fa";
+import { FaListCheck } from "react-icons/fa6";
 import { IoMdPeople, IoMdSettings } from "react-icons/io";
-import { MdBarChart, MdDashboard, MdEventNote, MdHowToVote, MdLogout } from "react-icons/md";
+import { MdBarChart, MdDashboard, MdEventNote, MdHowToVote, MdLogout, MdMonitor } from "react-icons/md";
 
 const listsidebar = {
+    // admin: [
+    //     {
+    //         id: "dashboard",
+    //         label: "Dashboard",
+    //         href: "/admin/dashboard",
+    //         icon: <MdDashboard className="w-6 h-6" />
+    //     },
+    //     {
+    //         id: "pilkades",
+    //         label: "Pilkades",
+    //         href: "/admin/election",
+    //         icon: <MdEventNote  className="w-6 h-6" />
+    //     },
+    //     {
+    //         id: "tps",
+    //         label: "Tps",
+    //         href: "/admin/tps",
+    //         icon: <MdHowToVote className="w-6 h-6" />
+    //     },
+    //     {
+    //         id: "petugas",
+    //         label: "Petugas",
+    //         href: "/admin/petugas",
+    //         icon: <IoMdPeople  className="w-6 h-6" />
+    //     },
+    //     {
+    //         id: "kandidat",
+    //         label: "Kandidat",
+    //         href: "/admin/candidate",
+    //         icon: <FaUserTie  className="w-6 h-6" />
+    //     },
+    //     {
+    //         id: "hasil",
+    //         label: "Hasil Suara",
+    //         href: "/admin/voting",
+    //         icon: <MdBarChart  className="w-6 h-6" />
+    //     },
+    //     {
+    //         id: "pengaturan",
+    //         label: "Pengaturan",
+    //         href: "/admin/settings",
+    //         icon: <IoMdSettings  className="w-6 h-6" />
+    //     }
+    // ],
+
+    // petugas: [
+    //     {
+    //         id: "dashboard",
+    //         label: "Dashboard",
+    //         href: "/petugas/dashboard",
+    //         icon: <MdDashboard className="w-6 h-6" />
+    //     },
+    //     {
+    //         id: "voter",
+    //         label: "Voter",
+    //         href: "/petugas/voter",
+    //         icon: <FaUsers className="w-6 h-6" />
+    //     },
+    //     {
+    //         id: "pengaturan",
+    //         label: "Pengaturan",
+    //         href: "/admin/dashboard",
+    //         icon: <IoMdSettings className="w-6 h-6" />
+    //     }
+    // ]
     admin: [
         {
-            id: "dashboard",
-            label: "Dashboard",
-            href: "/admin/dashboard",
-            icon: <MdDashboard className="w-8 h-8" />
+            id: "management",
+            label: "Management",
+            items: [
+                {
+                    id: "dashboard",
+                    label: "Dashboard",
+                    href: "/admin/dashboard",
+                    icon: <MdDashboard className="w-6 h-6" />
+                },
+                {
+                    id: "pilkades",
+                    label: "Pilkades",
+                    href: "/admin/election",
+                    icon: <MdEventNote  className="w-6 h-6" />
+                },
+                {
+                    id: "tps",
+                    label: "Tps",
+                    href: "/admin/tps",
+                    icon: <MdHowToVote className="w-6 h-6" />
+                },
+                {
+                    id: "petugas",
+                    label: "Petugas",
+                    href: "/admin/petugas",
+                    icon: <IoMdPeople  className="w-6 h-6" />
+                },
+                {
+                    id: "kandidat",
+                    label: "Kandidat",
+                    href: "/admin/candidate",
+                    icon: <FaUserTie  className="w-6 h-6" />
+                },
+                {
+                    id: "pengaturan",
+                    label: "Pengaturan",
+                    href: "/admin/settings",
+                    icon: <IoMdSettings  className="w-6 h-6" />
+                }
+            ]
         },
         {
-            id: "pilkades",
-            label: "Pilkades",
-            href: "/admin/election",
-            icon: <MdEventNote  className="w-8 h-8" />
-        },
-        {
-            id: "tps",
-            label: "Tps",
-            href: "/admin/tps",
-            icon: <MdHowToVote className="w-8 h-8" />
-        },
-        {
-            id: "petugas",
-            label: "Petugas",
-            href: "/admin/petugas",
-            icon: <IoMdPeople  className="w-8 h-8" />
-        },
-        {
-            id: "kandidat",
-            label: "Kandidat",
-            href: "/admin/candidate",
-            icon: <FaUserTie  className="w-8 h-8" />
-        },
-        {
-            id: "hasil",
-            label: "Hasil Suara",
-            href: "/admin/voting",
-            icon: <MdBarChart  className="w-8 h-8" />
-        },
-        {
-            id: "pengaturan",
-            label: "Pengaturan",
-            href: "/admin/settings",
-            icon: <IoMdSettings  className="w-8 h-8" />
+            id: "pemungutan",
+            label: "Pemungutan",
+            items: [
+                {
+                    id: "monitoring",
+                    label: "Monitoring",
+                    href: "/petugas/monitoring",
+                    icon: <MdMonitor className="w-6 h-6" />
+                },
+            ]
         }
     ],
 
     petugas: [
         {
-            id: "dashboard",
-            label: "Dashboard",
-            href: "/petugas/dashboard",
-            icon: <MdDashboard className="w-8 h-8" />
+            id: "management",
+            label: "Management",
+            items: [
+                {
+                    id: "dashboard",
+                    label: "Dashboard",
+                    href: "/petugas/dashboard",
+                    icon: <MdDashboard className="w-6 h-6" />
+                },
+                {
+                    id: "voter",
+                    label: "Data Voter",
+                    href: "/petugas/voter",
+                    icon: <FaUsers className="w-6 h-6" />
+                },
+                {
+                    id: "pengaturan",
+                    label: "Pengaturan",
+                    href: "/admin/dashboard",
+                    icon: <IoMdSettings className="w-6 h-6" />
+                }
+            ]
         },
         {
-            id: "voter",
-            label: "Voter",
-            href: "/petugas/voter",
-            icon: <FaUsers className="w-8 h-8" />
-        },
-        {
-            id: "pengaturan",
-            label: "Pengaturan",
-            href: "/admin/dashboard",
-            icon: <IoMdSettings className="w-8 h-8" />
+            id: "pemungutan",
+            label: "Pemungutan",
+            items: [
+                {
+                    id: "absensi",
+                    label: "Absensi & Token",
+                    href: "/petugas/absensi",
+                    icon: <FaListCheck className="w-6 h-6" />
+                },
+                {
+                    id: "voting",
+                    label: "Voting",
+                    href: "/petugas/voting",
+                    icon: <MdHowToVote className="w-6 h-6" />
+                },
+                {
+                    id: "monitoring",
+                    label: "Monitoring",
+                    href: "/petugas/monitoring",
+                    icon: <MdMonitor className="w-6 h-6" />
+                },
+            ]
         }
     ]
 }
